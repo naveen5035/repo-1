4c0dd8c58a5dd72f8f4d6d11f27db31806401637
naveen5035 <76766769+naveen5035@users.noreply.github.com>
2021-02-12 21:31:25 +0530

    Add files via upload

--- naveen/src/main/java/com/companyname/automobile/App.java
+++ naveen/src/main/java/com/companyname/automobile/App.java
@@ -1,13 +1,9 @@
 package com.companyname.automobile;
 
-/**
- * Hello world!
- *
- */
-public class App 
+class hello
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+   public static void main(String args[])
+   {
+     System.out.println("hello world");
+   }
 }
